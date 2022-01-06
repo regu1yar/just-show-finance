98ceaf4c7ef5783da86e56c332a497780c865db6
Roman Klimovitskiy <roma_klimovitskiy@mail.ru>
2022-01-06 14:34:33 +0300

    Created database, tinkoff-api and investments modules, added dependencies, setup flyway migrations, added grpc protobuf files and codegen

---
+++ java/investments/src/main/java/justshow/investments/InvestmentsApplication.java
@@ -0,0 +1,13 @@
+package justshow.investments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmentsApplication.class, args);
+	}
+
+}
