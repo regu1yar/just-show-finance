a57be2f11f94254973b6ecf60250c202cd262794
Roman Klimovitskiy <roma_klimovitskiy@mail.ru>
2022-01-09 12:53:05 +0300

    Added eureka service discovery, wrapped services into docker containers, added docker compose configuration

--- java/database/src/main/java/justshow/database/DatabaseApplication.java
+++ java/database/src/main/java/justshow/database/DatabaseApplication.java
@@ -2,8 +2,10 @@ package justshow.database;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DatabaseApplication {
 
     public static void main(String[] args) {
